4ad4f0243e0d8e18711c9fbed5efac883d148e35
mihaiv <mihaiv@yahoo-inc.com>
2014-09-06 13:17:28 -0400

    added 2d rotation / absolute position.  Fixed chaining.

--- Classes/BasicAnimation/HMBasicAnimation.h
+++ Classes/BasicAnimation/HMBasicAnimation.h
@@ -9,28 +9,36 @@
 #import <Foundation/Foundation.h>
 
 static NSString* const HMBasicAnimation_OPACITY       = @"opacity";
+static NSString* const HMBasicAnimation_ROTATION      = @"transform.rotation";
 static NSString* const HMBasicAnimation_ROTATION_Y    = @"transform.rotation.y";
 static NSString* const HMBasicAnimation_ROTATION_X    = @"transform.rotation.x";
 static NSString* const HMBasicAnimation_ROTATION_Z    = @"transform.rotation.z";
 static NSString* const HMBasicAnimation_SCALE_X       = @"transform.scale.x";
 static NSString* const HMBasicAnimation_SCALE_Y       = @"transform.scale.y";
 static NSString* const HMBasicAnimation_SCALE_Z       = @"transform.scale.z";
-static NSString* const HMBasicAnimation_TRANSLATION_X = @"transform.translation.x";
-static NSString* const HMBasicAnimation_TRANSLATION_Y = @"transform.translation.y";
-static NSString* const HMBasicAnimation_TRANSLATION_Z = @"transform.translation.z";
+static NSString* const HMBasicAnimation_TRANSLATION_X = @"transform.translation.x"; // relative change from current x
+static NSString* const HMBasicAnimation_TRANSLATION_Y = @"transform.translation.y"; // relative change from current y
+static NSString* const HMBasicAnimation_TRANSLATION_Z = @"transform.translation.z"; // relative change from current z
+static NSString* const HMBasicAnimation_POSITION_X    = @"position.x"; // center is registration point
+static NSString* const HMBasicAnimation_POSITION_Y    = @"position.y"; // center is registration point
 
 @interface HMBasicAnimation : NSObject
 
-+ (void)doRotate3DAnimation:(CALayer*)layer
-              toDegreeAngle:(float)toDegreeAngle
-                   duration:(CGFloat)duration
-               delaySeconds:(CGFloat)delaySeconds
-                    keyPath:(NSString*)keyPath;
++ (void)rotate2D:(CALayer*)layer
+   toDegreeAngle:(float)toDegreeAngle
+        duration:(CGFloat)duration
+    delaySeconds:(CGFloat)delaySeconds;
 
-+ (void)doAnimation:(CALayer*)layer
-            toValue:(id)toValue
-           duration:(CGFloat)duration
-       delaySeconds:(CGFloat)delaySeconds
-            keyPath:(NSString*)keyPath;
++ (void)rotate3D:(CALayer*)layer
+    toDegreeAngle:(float)toDegreeAngle
+         duration:(CGFloat)duration
+     delaySeconds:(CGFloat)delaySeconds
+          keyPath:(NSString*)keyPath;
+
++ (void)animate:(CALayer*)layer
+         toValue:(id)toValue
+        duration:(CGFloat)duration
+    delaySeconds:(CGFloat)delaySeconds
+         keyPath:(NSString*)keyPath;
 
 @end
--- Classes/BasicAnimation/HMBasicAnimation.m
+++ Classes/BasicAnimation/HMBasicAnimation.m
@@ -11,38 +11,45 @@
 
 @implementation HMBasicAnimation
 
-+ (void)doAnimation:(CALayer*)layer
-            toValue:(id)toValue
-           duration:(CGFloat)duration
-       delaySeconds:(CGFloat)delaySeconds
-            keyPath:(NSString*)keyPath
++ (void)animate:(CALayer*)layer
+         toValue:(id)toValue
+        duration:(CGFloat)duration
+    delaySeconds:(CGFloat)delaySeconds
+         keyPath:(NSString*)keyPath
 {
-    id currentValue             = [layer valueForKeyPath:keyPath];
-    CABasicAnimation* animation = [CABasicAnimation animationWithKeyPath:keyPath];
-    animation.fromValue         = currentValue;
-    animation.toValue           = toValue;
-    animation.duration          = duration;
-    animation.beginTime         = CACurrentMediaTime() + delaySeconds;
-    [layer addAnimation:animation forKey:keyPath]; // start animation
-    
     // on delay set the value to the model (animating presentation layer wont update model)
     [HMGrandCentralDispatch delayCallbackOnMainQueueWithDelay:delaySeconds block:^{
+        id currentValue = [layer valueForKeyPath:keyPath];
+        CABasicAnimation* animation = [CABasicAnimation animationWithKeyPath:keyPath];
+        animation.fromValue = currentValue;
+        animation.toValue = toValue;
+        animation.duration = duration;
+        [layer addAnimation:animation forKey:keyPath]; // start animation
         [layer setValue:toValue forKeyPath:keyPath];
     }];
 }
 
-+ (void)doRotate3DAnimation:(CALayer*)layer
-              toDegreeAngle:(float)toDegreeAngle
-                   duration:(CGFloat)duration
-               delaySeconds:(CGFloat)delaySeconds
-                    keyPath:(NSString*)keyPath
++ (void)rotate2D:(CALayer*)layer
+    toDegreeAngle:(float)toDegreeAngle
+         duration:(CGFloat)duration
+     delaySeconds:(CGFloat)delaySeconds
+{
+    float radian = [self degreeToRadian:toDegreeAngle];
+    [self animate:layer toValue:@(radian) duration:duration delaySeconds:delaySeconds keyPath:HMBasicAnimation_ROTATION];
+}
+
++ (void)rotate3D:(CALayer*)layer
+    toDegreeAngle:(float)toDegreeAngle
+         duration:(CGFloat)duration
+     delaySeconds:(CGFloat)delaySeconds
+          keyPath:(NSString*)keyPath
 {
     CATransform3D transform = CATransform3DIdentity;
-    transform.m34           = 1.0 / 500.0;
-    layer.transform         = transform;
-    
+    transform.m34 = 1.0 / 500.0;
+    layer.transform = transform;
+
     float radian = [self degreeToRadian:toDegreeAngle];
-    [self doAnimation:layer toValue:@(radian) duration:duration delaySeconds:delaySeconds keyPath:keyPath];
+    [self animate:layer toValue:@(radian) duration:duration delaySeconds:delaySeconds keyPath:keyPath];
 }
 
 #pragma mark : helpers
